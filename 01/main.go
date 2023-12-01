5a64b9bcee9ab5c682d639cd217b11aba450ffd2
Ryan Connell <7585908+RyanConnell@users.noreply.github.com>
2023-12-01 10:25:34 +0000

    Day 1

---
+++ 01/main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var validNumbers = map[string]int{
+	"one":   1,
+	"two":   2,
+	"three": 3,
+	"four":  4,
+	"five":  5,
+	"six":   6,
+	"seven": 7,
+	"eight": 8,
+	"nine":  9,
+}
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+		return
+	}
+	defer file.Close()
+
+	var sum int
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			continue
+		}
+		val, err := asCalibrationValue(line)
+		if err != nil {
+			fmt.Printf("Error: %v", err)
+			continue
+		}
+		sum += val
+		fmt.Printf("- %2d <= %q\n", val, line)
+	}
+	fmt.Printf("Sum: %d\n", sum)
+}
+
+func hasNumStrPrefix(str string) (int, bool) {
+	for numStr, val := range validNumbers {
+		if strings.HasPrefix(str, numStr) {
+			return val, true
+		}
+	}
+	return 0, false
+}
+
+func asCalibrationValue(str string) (int, error) {
+	check := func(idx int) (int, error) {
+		if str[idx] >= '0' && str[idx] <= '9' {
+			first, err := strconv.Atoi(string(str[idx]))
+			if err != nil {
+				return -1, err
+			}
+			return first, nil
+		}
+
+		val, ok := hasNumStrPrefix(str[idx:])
+		if !ok {
+			return -1, nil
+		}
+		return val, nil
+	}
+
+	var err error
+	first, last := -1, -1
+	for i := 0; i < len(str); i++ {
+		if first != -1 && last != -1 {
+			break
+		}
+		if first == -1 {
+			first, err = check(i)
+			if err != nil {
+				return -1, err
+			}
+		}
+		if last == -1 {
+			last, err = check(len(str) - 1 - i)
+			if err != nil {
+				return -1, err
+			}
+		}
+	}
+
+	return (first * 10) + last, nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/RyanConnell/advent-of-code
+
+go 1.19
