50a90d057c241646b6f90bc239b38432c988f5a7
Ryan Connell <7585908+RyanConnell@users.noreply.github.com>
2023-12-14 23:06:01 +0000

    Day 13: part 2

--- 13/main.go
+++ 13/main.go
@@ -9,14 +9,14 @@ import (
 func main() {
 	lines := parser.MustReadFile("input/input.txt")
 
-	solutionPart1, err := solve(lines)
+	solutionPart1, err := solve(lines, false)
 	if err != nil {
 		fmt.Printf("Error: %v", err)
 		return
 	}
 	fmt.Printf("Part 1 Result: %d\n", solutionPart1)
 
-	solutionPart2, err := solvePart2(lines)
+	solutionPart2, err := solve(lines, true)
 	if err != nil {
 		fmt.Printf("Error: %v", err)
 		return
@@ -25,13 +25,14 @@ func main() {
 }
 
 type Pattern struct {
-	lines []string
+	lines           []string
+	hasHiddenMirror bool
 }
 
 func (p *Pattern) lineOfSymmetry() (int, int) {
-	horizontal := findLineOfSymmetry(p.lines)
+	horizontal := findLineOfSymmetry(p.lines, p.hasHiddenMirror)
 	rotated := rotate(p.lines)
-	vertical := findLineOfSymmetry(rotated)
+	vertical := findLineOfSymmetry(rotated, p.hasHiddenMirror)
 	return vertical, horizontal
 }
 
@@ -45,31 +46,41 @@ func rotate(lines []string) []string {
 	return rotated
 }
 
-func findLineOfSymmetry(lines []string) int {
+func findLineOfSymmetry(lines []string, allowOffByOne bool) int {
 	// Vertical symmetry will always have a situation where the first line or the last line
 	// are at the edge of the symmetry.
 	positions := [][]int{
 		{}, // Represents the indexes that match line[0]
 		{}, // Represents the indexes that match line[-1]
 	}
+
+	var maxDiff int
+	if allowOffByOne {
+		maxDiff = 1
+	}
+
 	for i, line := range lines {
-		if i != 0 && line == lines[0] {
-			positions[0] = append(positions[0], i)
+		if i != 0 {
+			if lineDifferences(line, lines[0]) <= maxDiff {
+				positions[0] = append(positions[0], i)
+			}
 		}
-		if i != len(lines)-1 && line == lines[len(lines)-1] {
-			positions[1] = append(positions[1], i)
+		if i != len(lines)-1 {
+			if lineDifferences(line, lines[len(lines)-1]) <= maxDiff {
+				positions[1] = append(positions[1], i)
+			}
 		}
 	}
 
 	// For each potential match check if the symmetry works out.
 	for _, pos := range positions[0] {
-		if checkSymmetry(lines, pos/2) {
+		if checkSymmetry(lines, pos/2, allowOffByOne) {
 			return pos / 2
 		}
 	}
 	for _, pos := range positions[1] {
 		midpoint := pos + ((len(lines) - 1 - pos) / 2)
-		if checkSymmetry(lines, midpoint) {
+		if checkSymmetry(lines, midpoint, allowOffByOne) {
 			return midpoint
 		}
 	}
@@ -77,57 +88,56 @@ func findLineOfSymmetry(lines []string) int {
 }
 
 // Midpoint always refers to the N where we suspect (lines[N] == lines[N+1])
-func checkSymmetry(lines []string, midpoint int) bool {
-	fmt.Printf("CheckSymmetry: %d: ", midpoint)
-	for i := 0; i < midpoint; i++ {
+func checkSymmetry(lines []string, midpoint int, allowOffByOne bool) bool {
+	var maxDiff int
+	if allowOffByOne {
+		maxDiff = 1
+	}
+	for i := 0; i <= midpoint; i++ {
 		if midpoint-i < 0 || midpoint+1+i >= len(lines) {
 			break
 		}
-		if lines[midpoint-i] != lines[midpoint+1+i] {
-			fmt.Printf("false\n")
+		maxDiff -= lineDifferences(lines[midpoint-i], lines[midpoint+1+i])
+		if maxDiff < 0 {
 			return false
 		}
 	}
-	fmt.Printf("true\n")
-	return true
+	return maxDiff == 0
 }
 
-/// Part 1 \\\
-
-func parsePatterns(lines []string) []*Pattern {
+func parsePatterns(lines []string, hasHiddenMirror bool) []*Pattern {
 	var lastPatternBreak int
 	var patterns []*Pattern
 	for i, line := range lines {
 		if line != "" {
 			continue
 		}
-		patterns = append(patterns, &Pattern{lines[lastPatternBreak:i]})
+		patterns = append(patterns, &Pattern{lines[lastPatternBreak:i], hasHiddenMirror})
 		lastPatternBreak = i + 1
 	}
-	return append(patterns, &Pattern{lines[lastPatternBreak:]})
+	return append(patterns, &Pattern{lines[lastPatternBreak:], hasHiddenMirror})
 }
 
-func solve(lines []string) (int, error) {
-	patterns := parsePatterns(lines)
+func solve(lines []string, hasSmudge bool) (int, error) {
+	patterns := parsePatterns(lines, hasSmudge)
 	var sum int
-	for i, pattern := range patterns {
-		fmt.Printf("Pattern Length: %d\n", len(pattern.lines))
+	for _, pattern := range patterns {
 		verticalPos, horizontalPos := pattern.lineOfSymmetry()
 		if verticalPos != -1 {
 			sum += verticalPos + 1
-			fmt.Printf("Pattern %d: vertical @ %d\n", i, verticalPos+1)
 		} else if horizontalPos != -1 {
 			sum += 100 * (horizontalPos + 1)
-			fmt.Printf("Pattern %d: horizontal @ %d\n", i, horizontalPos+1)
-		} else {
-			fmt.Printf("WARNING: Pattern %d had no horizontal or vertical symmetry\n", i)
 		}
 	}
 	return sum, nil
 }
 
-/// Part 2 \\\
-
-func solvePart2(lines []string) (int, error) {
-	return 0, nil
+func lineDifferences(line1, line2 string) int {
+	var count int
+	for i := 0; i < len(line1); i++ {
+		if line1[i] != line2[i] {
+			count++
+		}
+	}
+	return count
 }
--- 13/solution_test.go
+++ 13/solution_test.go
@@ -6,13 +6,31 @@ import (
 	"github.com/RyanConnell/aoc-23/pkg/tester"
 )
 
-func TestDay00(t *testing.T) {
-	tester.TimeAndCheck(0, []tester.TestCase[int]{
+func TestDay13(t *testing.T) {
+	tester.TimeAndCheck(13, []tester.TestCase[int]{
 		{
 			Description: "Part 1 (sample)",
 			File:        "sample.txt",
-			Solution:    solve,
-			Expected:    0,
+			Solution:    func(lines []string) (int, error) { return solve(lines, false) },
+			Expected:    405,
+		},
+		{
+			Description: "Part 1 (final)",
+			File:        "input.txt",
+			Solution:    func(lines []string) (int, error) { return solve(lines, false) },
+			Expected:    29165,
+		},
+		{
+			Description: "Part 2 (sample)",
+			File:        "sample.txt",
+			Solution:    func(lines []string) (int, error) { return solve(lines, true) },
+			Expected:    400,
+		},
+		{
+			Description: "Part 2 (final)",
+			File:        "input.txt",
+			Solution:    func(lines []string) (int, error) { return solve(lines, true) },
+			Expected:    32192,
 		},
 	})
 }
